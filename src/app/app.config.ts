837ba69542f580f28f5c398c9711d0ffaecc1960
sandipsky <sandipshakya75@gmail.com>
2025-10-17 17:02:54 +0545

    firebase init

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,21 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase: {
+        apiKey: "AIzaSyCzwdewrnfSN-iBq8SJCLoaXM2khrCTYFk",
+        authDomain: "chess-game-80f69.firebaseapp.com",
+        projectId: "chess-game-80f69",
+        storageBucket: "chess-game-80f69.firebasestorage.app",
+        messagingSenderId: "678430260237",
+        appId: "1:678430260237:web:acd0f8b33919bb130a2944",
+        measurementId: "G-MEWY0YQWEE"
+    }
+}
